94a85725461374b9b871257696c2d9b7ea0deeb5
Nguyễn Văn Tiến <54173812+Nightmare457@users.noreply.github.com>
2021-08-14 11:27:15 +0700

    info

---
+++ SPV201_TEAM01/SPV201_TEAM01/InfoForm.Designer.cs
@@ -0,0 +1,91 @@
+﻿
+namespace SPV201_TEAM01
+{
+    partial class InfoForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBoxOptions = new System.Windows.Forms.ComboBox();
+            this.labelOptions = new System.Windows.Forms.Label();
+            this.dataGridViewInfo = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewInfo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBoxOptions
+            // 
+            this.comboBoxOptions.FormattingEnabled = true;
+            this.comboBoxOptions.Items.AddRange(new object[] {
+            "Contestants",
+            "Examiners",
+            "Official members",
+            "Backup members"});
+            this.comboBoxOptions.Location = new System.Drawing.Point(173, 29);
+            this.comboBoxOptions.Name = "comboBoxOptions";
+            this.comboBoxOptions.Size = new System.Drawing.Size(190, 21);
+            this.comboBoxOptions.TabIndex = 0;
+            this.comboBoxOptions.SelectedValueChanged += new System.EventHandler(this.SelectedValueChanged);
+            // 
+            // labelOptions
+            // 
+            this.labelOptions.AutoSize = true;
+            this.labelOptions.Location = new System.Drawing.Point(37, 29);
+            this.labelOptions.Name = "labelOptions";
+            this.labelOptions.Size = new System.Drawing.Size(41, 13);
+            this.labelOptions.TabIndex = 1;
+            this.labelOptions.Text = "Option:";
+            // 
+            // dataGridViewInfo
+            // 
+            this.dataGridViewInfo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewInfo.Location = new System.Drawing.Point(24, 84);
+            this.dataGridViewInfo.Name = "dataGridViewInfo";
+            this.dataGridViewInfo.Size = new System.Drawing.Size(748, 354);
+            this.dataGridViewInfo.TabIndex = 2;
+            // 
+            // InfoForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridViewInfo);
+            this.Controls.Add(this.labelOptions);
+            this.Controls.Add(this.comboBoxOptions);
+            this.Name = "InfoForm";
+            this.Text = "InfoForm";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewInfo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxOptions;
+        private System.Windows.Forms.Label labelOptions;
+        private System.Windows.Forms.DataGridView dataGridViewInfo;
+    }
+}
---
+++ SPV201_TEAM01/SPV201_TEAM01/InfoForm.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace SPV201_TEAM01
+{
+    public partial class InfoForm : Form
+    {
+        private string sConnection;
+        private SqlConnection cnn;
+
+        public InfoForm()
+        {
+            InitializeComponent();
+            sConnection = System.Configuration.ConfigurationSettings.AppSettings["DB"].ToString();
+            cnn = new SqlConnection(sConnection);
+        }
+
+        private void SelectedValueChanged(object sender, EventArgs e)
+        {
+            if (comboBoxOptions.SelectedItem.ToString() == "Contestants")
+            {
+                LoadAllContestants();
+            }
+            else if (comboBoxOptions.SelectedItem.ToString() == "Examiners")
+            {
+                LoadAllExaminers();
+            }
+            else if (comboBoxOptions.SelectedItem.ToString() == "Official members")
+            {
+                LoadAllOfficialMembers();
+            }
+            else if (comboBoxOptions.SelectedItem.ToString() == "Backup members")
+            {
+                LoadAllBackupMembers();
+            }
+        }
+
+        private void LoadAllContestants()
+        {
+            cnn.Open();
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = cnn;
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "sp_GetAllContestants";
+
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+
+            DataSet dataset = new DataSet();
+            adapter.Fill(dataset);
+
+            if (dataset.Tables.Count > 0)
+            {
+                dataGridViewInfo.DataSource = dataset.Tables[0];
+            }
+
+            cnn.Close();
+        }
+
+        private void LoadAllExaminers()
+        {
+            cnn.Open();
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = cnn;
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "sp_GetAllExaminers";
+
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+
+            DataSet dataset = new DataSet();
+            adapter.Fill(dataset);
+
+            if (dataset.Tables.Count > 0)
+            {
+                dataGridViewInfo.DataSource = dataset.Tables[0];
+            }
+
+            cnn.Close();
+        }
+
+        private void LoadAllOfficialMembers()
+        {
+            cnn.Open();
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = cnn;
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "sp_GetAllOfficalMembers";
+
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+
+            DataSet dataset = new DataSet();
+            adapter.Fill(dataset);
+
+            if (dataset.Tables.Count > 0)
+            {
+                dataGridViewInfo.DataSource = dataset.Tables[0];
+            }
+
+            cnn.Close();
+        }
+
+        private void LoadAllBackupMembers()
+        {
+            cnn = new SqlConnection(sConnection);
+            cnn.Open();
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = cnn;
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "sp_GetAllBackupMembers";
+
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+
+            DataSet dataset = new DataSet();
+            adapter.Fill(dataset);
+
+            if (dataset.Tables.Count > 0)
+            {
+                dataGridViewInfo.DataSource = dataset.Tables[0];
+            }
+
+            cnn.Close();
+        }
+
+        private void Close_InfoForm(object sender, FormClosedEventArgs e)
+        {
+
+        }
+    }
+}
